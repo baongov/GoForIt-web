6cfcd390bfd9341333e00623cdd670832055e757
OKATA <ngothivietbao@gmail.com>
2016-11-29 14:36:09 +0700

    login by facebook

--- Source.sql
+++ Source.sql
@@ -9,8 +9,11 @@ CREATE TABLE `Users` (
 `id` int(10) NOT NULL auto_increment,
 `idFacebook` varchar(40) NOT NULL,
 `displayName` varchar(40) NOT NULL,
-`gender` char(1) enum('M','F') NOT NULL,
+`photo` varchar(200),
+`email` varchar(100),
+`gender` char(6) NOT NULL,
 `address` varchar(50) default '',
-`age` int(2) NOT NULL,
+`age` int(2),
 PRIMARY KEY (`id`)
 );
+INSERT INTO Users SET `idFacebook` = '1152419461324591', `displayName` = 'Anonimity', `photo` = NULL, `email` = 'anonimity@xyz.com', `gender` = 'female', `address` = NULL, `age` = 18;
